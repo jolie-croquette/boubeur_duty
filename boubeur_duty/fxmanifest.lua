51b356b4a2c7b3a8aba0c1a02e5c83af61426d0b
Jolie_Croquette <83880530+jolie-croquette@users.noreply.github.com>
2023-04-03 20:33:37 -0400

    Create fxmanifest.lua
    The manifest file was missing | Le fichier manifest était manquant

---
+++ boubeur_duty/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'boubeur'
+description 'Duty Ressource'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
+
+server_script {
+    'server.lua'
+}
